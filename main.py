bfe3b2071a2561062f317bc3d0d70274bb28bda0
poojamounika <poojamounika6608@gmail.com>
2025-03-21 11:54:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title('Hello World')
 st.write('this is a simple Streamlit app.')
+if st.button('Say Hello'):
+		st.text('Hello streamlit')
+		
+name=st.text_input ('Please Enter Your Name :')
+
+f name:
+	st.write(f'Hello,{name}!')
+	
+	if st.file_uploder('Please upload a file : ',type=['txt','cvs']):
+	st.write('Thanks for uploading a file')
